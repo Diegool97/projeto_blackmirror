49ef922419268bfe348d189841117e429a8614ef
Diegool97 <113364521+Diegool97@users.noreply.github.com>
2024-03-28 11:05:58 -0300

    Add files via upload
    Atualizando

--- atividade_black_mirrior.py
+++ atividade_black_mirrior.py
@@ -7,44 +7,52 @@
 # QUESTIONÁRIO ANTERIOR) E O
 # SISTEMA DEVERÁ DAR A RESPOSTA.
 
-pergunta =''' 
+pergunta1 = "[1] QUAL É O NOME COMPLETO DA PROTAGONISTA DO EPISÓDIO?"
+pergunta2 = "[2] QUEM DIRIGE A VIDA DE JOAN EM TEMPO REAL PARA UMA SÉRIE DE TELEVISÃO?"
+pergunta3 = "[3] QUAL É O NOME DO SERVIÇO DE STREAMING FICTÍCIO QUE PARODIA A NETFLIX NO EPISÓDIO?"
+pergunta4 = "[4] COMO JOAN DESCOBRE A EXISTÊNCIA DA SÉRIE SOBRE SUA VIDA?"
+pergunta5 = "[5] QUAL É A REAÇÃO INICIAL DE JOAN AO DESCOBRIR A SÉRIE E O QUE ELA FAZ EM RESPOSTA?"
+pergunta6 = "[6] QUAIS SÃO OS TEMAS PRINCIPAIS EXPLORADOS NESTE EPISÓDIO DE BLACK MIRROR?"
+pergunta7 = "[7] O EPISÓDIO TERMINA DE MANEIRA TÍPICA PARA A SÉRIE BLACK MIRROR? EXPLIQUE."
+
+pergunta =f''' 
 =========================================================================================================
                  ESCOLHA UMA DAS PERGUNTAS DA SÉRIE PARA SABER A RESPOSTA:
  
-        [1] QUAL É O NOME COMPLETO DA PROTAGONISTA DO EPISÓDIO? 
+        {pergunta1}
 
-        [2] QUEM DIRIGE A VIDA DE JOAN EM TEMPO REAL PARA UMA SÉRIE DE TELEVISÃO? 
+        {pergunta2}
 
-        [3] QUAL É O NOME DO SERVIÇO DE STREAMING FICTÍCIO QUE PARODIA A NETFLIX NO EPISÓDIO? 
+        {pergunta3}
 
-        [4] COMO JOAN DESCOBRE A EXISTÊNCIA DA SÉRIE SOBRE SUA VIDA? 
+        {pergunta4}
 
-        [5] QUAL É A REAÇÃO INICIAL DE JOAN AO DESCOBRIR A SÉRIE E O QUE ELA FAZ EM RESPOSTA? 
+        {pergunta5}
 
-        [6] QUAIS SÃO OS TEMAS PRINCIPAIS EXPLORADOS NESTE EPISÓDIO DE BLACK MIRROR? 
+        {pergunta6}
 
-        [7] O EPISÓDIO TERMINA DE MANEIRA TÍPICA PARA A SÉRIE BLACK MIRROR? EXPLIQUE.        
+        {pergunta7}
                  
   INFORME A OPÇÃO ESCOLHIDA: 
 '''
-
+voltar = "Deseja realizar uma nova pergunta? (S/N)"
 
 while True:
-      opcaoEscolhida = input(pergunta)   
+     opcaoEscolhida = input(pergunta)   
      if opcaoEscolhida == "1":
-         print("Resposta da Pergunta 1: O nome da protagonista é Joan")
+         print(f"{pergunta1} O nome da protagonista é Joan")
      elif opcaoEscolhida == "2": 
-         print("Resposta da Pergunta 2: Moana é quem dirige a vida de Joan")
+         print(f"{pergunta2} Moana é quem dirige a vida de Joan")
      elif opcaoEscolhida == "3": 
-         print("Resposta da Pergunta 3: Streamberry é o serviço de streaming")
+         print(f"{pergunta3} Streamberry é o serviço de streaming")
      elif opcaoEscolhida == "4": 
-         print("Resposta da Pergunta 4: Joan descobre no sofá de casa")
+         print(f"{pergunta4} Joan descobre no sofá de casa")
      elif opcaoEscolhida == "5": 
-         print("Resposta da Pergunta 5: Uma sensação de Desespero")
+         print(f"{pergunta5} Uma sensação de Desespero")
      elif opcaoEscolhida == "6": 
-         print("Resposta da Pergunta 6: Traição, trabalho e emoção são os temas explorados")
+         print(f"{pergunta6} Traição, trabalho e emoção são os temas explorados")
      elif opcaoEscolhida == "7": 
-         print("Resposta da Pergunta 7: Não")
+         print(f"{pergunta7} Não")
          break
      else:
          print("OPÇÃO INVÁLIDA") 
